5884a1390d3c27f7eca0316bf62e0550c6ef6083
mwanginjamb <fnjambi@outlook.com>
2026-09-10 10:12:09 +0300

    Refactor: Add icon and descriptive metadata

--- frontend/views/layouts/_head.php
+++ frontend/views/layouts/_head.php
@@ -7,6 +7,8 @@ declare(strict_types=1);
 use frontend\assets\AppAsset;
 
 AppAsset::register($this);
+$this->params['meta_description'] = 'BioSketch Professional is a web application that allows researchers to create and manage their biosketches for grant applications. It provides a user-friendly interface to input personal information, education, work experience, publications, and other relevant details. The application also generates formatted biosketch documents that comply with funding agency requirements.';
+$this->params['meta_keywords'] = 'BioSketch, Professional, Researcher, Biosketch, Grant Applications, Web Application, User-Friendly Interface, Personal Information, Education, Work Experience, Publications, Formatted Documents, Funding Agency Requirements';
 
 
 $this->registerCsrfMetaTags();
@@ -39,7 +41,9 @@ if (!empty($this->params['meta_keywords'])) {
 $this->registerLinkTag(
     [
         'rel' => 'icon',
-        'type' => 'image/x-icon',
-        'href' => Yii::getAlias('@web/favicon.ico'),
+        'type' => 'image/png',
+        'href' => Yii::getAlias('@web/icons/icon-32x32.png'),
     ],
 );
+
+
